25cf9c2a0a85d658a56fa998c7d36c5550b2408c
Jalen Evans Moore <125609120+Jralixs@users.noreply.github.com>
2023-06-15 17:17:04 -0500

    remove previous change

--- Main.java
+++ Main.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Result {
-    public static List<Integer> reverseArray(int arraySize, List<Integer> a) {
-        List <Integer> result = new ArrayList<>();
-        for (int i = 0; i < arraySize); i++)
-            result.add(a.get(arraySize) - (1+i)));
+    public static List<Integer> reverseArray(List<Integer> a) {
+        List <Integer> result;
+        result = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++)
+            result.add(a.get(a.size() - (1+i)));
         return result;
     }
 
