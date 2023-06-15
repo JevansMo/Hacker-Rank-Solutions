e177fbfc17f9319aaf89b81e22287e3625ca7030
Jalen Evans Moore <125609120+Jralixs@users.noreply.github.com>
2023-06-15 18:13:45 -0500

    add context given by hacker rank

--- Main.java
+++ Main.java
@@ -3,8 +3,18 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+
 class Result {
+
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
     public static List<Integer> reverseArray(List<Integer> a) {
+        // Write your code here
         List <Integer> result;
         result = new ArrayList<>();
         for (int i = 0; i < a.size(); i++)
