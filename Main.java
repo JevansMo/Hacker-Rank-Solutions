19917c8ca025decb064997d24b11956fe1ed743c
Jalen Evans Moore <125609120+Jralixs@users.noreply.github.com>
2023-06-15 16:39:28 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Result {
+    public static List<Integer> reverseArray(List<Integer> a) {
+        List <Integer> result = new ArrayList<>();
+        for (int i = 0; i < a.size(); i++)
+            result.add(a.get(a.size() - (1+i)));
+        return result;
+    }
+
+}
